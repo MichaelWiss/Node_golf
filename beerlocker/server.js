8069445949c81027cb15a73d841a50ba5e17f61e
Michael Wiss <michael.wiss@gmail.com>
2015-10-09 20:04:09 -0500

    post

--- beerlocker/server.js
+++ beerlocker/server.js
@@ -1,4 +1,4 @@
-// Get the packages we need
+// Load required packages
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
@@ -24,10 +24,8 @@ var router = express.Router();
 // Initial dummy route for testing
 // http://localhost:3000/api
 router.get('/', function(req, res) {
-  res.json({ message: 'You are running dangerously low on beer!' });
+  res.json({ message: 'You are running dangerously low on beer!' }); 
 });
-
-// -- New Code Below Here -- //
 
 // Create a new route with the prefix /beers
 var beersRoute = router.route('/beers');
@@ -48,6 +46,62 @@ beersRoute.post(function(req, res) {
       res.send(err);
 
     res.json({ message: 'Beer added to the locker!', data: beer });
+  });
+});
+
+// Create endpoint /api/beers for GET
+beersRoute.get(function(req, res) {
+  // Use the Beer model to find all beer
+  Beer.find(function(err, beers) {
+    if (err)
+      res.send(err);
+
+    res.json(beers);
+  });
+});
+
+// Create a new route with the /beers/:beer_id prefix
+var beerRoute = router.route('/beers/:beer_id');
+
+// Create endpoint /api/beers/:beer_id for GET
+beerRoute.get(function(req, res) {
+  // Use the Beer model to find a specific beer
+  Beer.findById(req.params.beer_id, function(err, beer) {
+    if (err)
+      res.send(err);
+
+    res.json(beer);
+  });
+});
+
+// Create endpoint /api/beers/:beer_id for PUT
+beerRoute.put(function(req, res) {
+  // Use the Beer model to find a specific beer
+  Beer.findById(req.params.beer_id, function(err, beer) {
+    if (err)
+      res.send(err);
+
+    // Update the existing beer quantity
+    beer.quantity = req.body.quantity;
+
+    // Save the beer and check for errors
+    beer.save(function(err) {
+      if (err)
+        res.send(err);
+
+      res.json(beer);
+    });
+  });
+});
+
+// Create endpoint /api/beers/:beer_id for DELETE
+beerRoute.delete(function(req, res) {
+  // Use the Beer model to find a specific beer and remove it
+  Beer.findByIdAndRemove(req.params.beer_id, function(err) {
+    if (err)
+      res.send(err);
+
+    res.json({ message: 'Beer removed from the locker!' });
   });
 });
 
